1bf5c958dad2df5f48db334c3d0d9079eabea6a7
yuuichi <u1sano0807@gmail.com>
2023-08-25 20:25:41 +0900

    [add]ユーザーの新規作成画面仮作成。デザインは未実装。エラーメッセージの表示機能作成。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,21 @@
 class UsersController < ApplicationController
+  def show
+  end
+
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
-  end
-
-  def show
   end
 
   private
