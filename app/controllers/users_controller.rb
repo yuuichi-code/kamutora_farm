b65c97181e9c2382e78dcd72a413e0d75417e66d
yuuichi <u1sano0807@gmail.com>
2023-08-26 00:43:36 +0900

    [add]ユーザー新規登録ページのテストコード作成。フラッシュメッセージの作成。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path
+      redirect_to root_path, notice: t('.success')
     else
+      flash.now[:danger] = t('.fail')
       render :new, status: :unprocessable_entity
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,17 +10,17 @@ RSpec.describe User, type: :model do
     it 'nameの値がない場合NG' do
       user = build(:user, name: nil)
       user.valid?
-      expect(user.errors[:name]).to include('を入力してください')
+      expect(user.errors[:name]).to eq I18n.t('errors.messages.blank')
     end
     it 'emailの値がない場合NG' do
       user = build(:user, email: nil)
       user.valid?
-      expect(user.errors[:email]).to include('を入力してください')
+      expect(user.errors[:email]).to eq I18n.t('errors.messages.blank')
     end
-    it 'passwordの値が2文字以下の場合NG' do
-      user = build(:user, password: 'pa')
+    it 'passwordの値が5文字以下の場合NG' do
+      user = build(:user, password: 'passw')
       user.valid?
-      expect(user.errors[:password]).to include('は3文字以上で入力してください')
+      expect(user.errors[:password]).to eq I18n.t('errors.messages.blank')
     end
   end
 end
