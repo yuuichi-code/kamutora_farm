d3091cf4a51f420cf6a4d873dbba1595ef7cf861
yuuichi <u1sano0807@gmail.com>
2023-08-28 21:44:28 +0900

    [add]ログイン前のヘッダーを作成。アイコンを利用できるように設定追加。

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'header-image': "url('header_sky.jpg')",
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
