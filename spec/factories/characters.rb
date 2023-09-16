89d6ac3b3ca60eb7d44b047e2be7728e25795bf9
yuuichi <u1sano0807@gmail.com>
2023-09-16 10:30:38 +0900

    [fix]ログインをクリックした後root_pathに遷移するまで待機するように修正

--- spec/factories/characters.rb
+++ spec/factories/characters.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :character do
-    sequence(:name) { |n| "test_name_#{n}" }
+    sequence(:name) { |n| "test_character_#{n}" }
     unit_element { :fire }
     initial_rarity { 4 }
   end
