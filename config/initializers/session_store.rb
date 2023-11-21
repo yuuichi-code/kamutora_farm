0c9bdf3563dc3945824e6c0a0801c45185ff6716
yuuichi <u1sano0807@gmail.com>
2023-11-21 21:26:08 +0900

    [fix]fly.ioのRedisに接続できるように変更

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,6 @@
 Rails.application.config.session_store :redis_store, expire_after: 1.day, servers: {
-  host: "localhost",
+  host: "fly-kamutora-farm-redis.upstash.io",
   port: 6379,
+  password: "f4fbc6c43a30474786123aa2fd0877dd",
   namespace: "user_sessions"
 }
